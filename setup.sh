a36edd3c47e0dd4641b139ad916ac54407da2052
Mattia De Vivo <mattiadevivo@gmail.com>
2026-03-27 17:45:59 +0100

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export TF_VAR_github_token=<github-token>
+cd terraform/fluxcd-bootstrap
 terraform init
-terraform apply                                                                                                                                                                                                                                                           
+terraform apply                                                                                                                                                                                                                                                      
 
